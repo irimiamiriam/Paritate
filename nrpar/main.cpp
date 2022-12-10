5acf47430361f340b377a2264af3b7e340da3099
IrimiaMiriam <irimiamiriam100@gmail.com>
2022-12-10 14:40:11 -0700

    Program pentru determinarea paritatii unui numar

---
+++ nrpar/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >>n;
+    if(n%2==0){cout <<"Numarul este par.";}
+    else cout <<"Numarul este impar.";
+    return 0;
+}
